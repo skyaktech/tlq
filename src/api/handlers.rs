48612ab47455f2f7660c7f0f2c7ebbebf53abefb
Nebojsa Jakovljevic <nebojsa@nebjak.net>
2025-03-23 19:21:53 +0100

    fix: reformat

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -1,14 +1,17 @@
+use crate::api::models::AddMessageRequest;
+use crate::services::MessageService;
+use crate::types::Message;
 use axum::extract::State;
 use axum::Json;
 use skyak_axum_core::errors::ApiError;
 use skyak_axum_core::https::{error, success, ApiResponse};
-use crate::api::models::AddMessageRequest;
-use crate::services::MessageService;
-use crate::types::Message;
 
-pub async fn add_message(State(service): State<MessageService>, Json(request): Json<AddMessageRequest>) -> ApiResponse<String> {
+pub async fn add_message(
+    State(service): State<MessageService>,
+    Json(request): Json<AddMessageRequest>,
+) -> ApiResponse<String> {
     dbg!(&request);
-    
+
     match service.add(Message::new(request.body)).await {
         Ok(_) => success("Success".to_string()),
         Err(_) => error(ApiError::BadRequest(None)),
